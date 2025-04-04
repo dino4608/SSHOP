214657d54c5bc1f5cb049b237c84df014e27ea34
tn9th8 <tn9th8@gmail.com>
2025-04-04 19:00:56 +0700

    kick off the buyer app

---
+++ apps/buyer/src/app/(auth)/login/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Page: React.FC = () => {
+    console.log(">>> Login page rendered");
+    return (
+        <div>
+            <h1>Login Page</h1>
+        </div>
+    );
+};
+
+export default Page;
