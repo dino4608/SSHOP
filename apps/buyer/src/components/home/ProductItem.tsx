bab7da2de6ac3b21b8f16888207bc57026e3922d
tn9th8 <tn9th8@gmail.com>
2025-04-09 22:58:29 +0700

    update

--- apps/buyer/src/components/home/ProductItem.tsx
+++ apps/buyer/src/components/home/ProductItem.tsx
@@ -17,20 +17,20 @@ const ProductItem = () => {
 
             <div className="px-2 lg:px-4 py-2 lg:py-4 space-y-1 lg:space-y-2">
                 {/* Product title */}
-                <div className="text-[10px] lg:text-base text-gray-800 line-clamp-2 ">
+                <div className="text-sm lg:text-base text-gray-800 line-clamp-2 ">
                     Product title [Demo with a pro max plus mega super ultra extra long text]
                 </div>
                 {/* Product price */}
                 <div className="flex items-center space-x-1 lg:space-x-2">
-                    <span className="text-base lg:text-[22px] text-red-500 font-semibold ">
+                    <span className="text-lg lg:text-xl text-red-500 font-semibold ">
                         99.99₫
                     </span>
-                    <span className="text-[8px] lg:text-sm text-gray-400 line-through ">
+                    <span className="text-[12px] lg:text-sm text-gray-400 line-through ">
                         200.00₫
                     </span>
                 </div>
                 {/* Free shipping */}
-                <div className="text-[8px] lg:text-sm  space-x-1 lg:space-x-2">
+                <div className="text-[12px] lg:text-sm space-x-1 lg:space-x-2">
                     <span className="inline-flex items-center justify-center px-2 bg-teal-100 rounded-sm text-teal-600 font-semibold">
                         Free shipping
                     </span>
@@ -39,7 +39,7 @@ const ProductItem = () => {
                     </span>
                 </div>
                 {/* Stars and sales */}
-                <div className="text-[8px] lg:text-sm text-gray-400">
+                <div className="text-[12px] lg:text-sm text-gray-400">
                     ⭐4.9 | 🔥600 sold
                 </div>
             </div>
