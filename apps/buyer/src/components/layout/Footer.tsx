a2ff202ea29a82abcf8baea7fb90e5fdafb75d13
tn9th8 <tn9th8@gmail.com>
2025-04-10 23:28:57 +0700

    stick the action column

--- apps/buyer/src/components/layout/Footer.tsx
+++ apps/buyer/src/components/layout/Footer.tsx
@@ -5,7 +5,7 @@ import React from 'react';
 
 const Footer: React.FC = () => {
     return (
-        <div className="w-full bg-black text-white h-80 p-8 sm:p-12 lg:p-20">
+        <div className="w-full bg-black text-white h-400 p-8 sm:p-12 lg:p-20">
             <div className="container h-full flex justify-center items-center mx-auto px-4 sm:px-20">
                 <p>&copy; {new Date().getFullYear()} {app.name}. All rights reserved.</p>
             </div>
